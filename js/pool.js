81e4714226686b15dc51c17375ee0e20e1916e5c
Soumith Chintala <soumith@fb.com>
2015-06-12 19:48:06 -0400

    first maxpool

--- js/pool.js
+++ js/pool.js
@@ -13,49 +13,39 @@ SpatialMaxPooling.prototype.forward(input) {
     var nPlane = input.shape[0];
     var iH = input.shape[1];
     var iW = input.shape[2];
-    var oH = Math.floor((iH - this.kH) / this.dH + 1);
-    var oW = Math.floor((iW - this.kW) / this.dW + 1);
+    var kH = this.kH
+    var kW = this.kW
+    var oH = Math.floor((iH - kH) / dH + 1);
+    var oW = Math.floor((iW - kW) / dW + 1);
 
-    var output = ndarray(new Float32Array(nPlane * oH * oW), 
-			 [nPlane, oH, oW]);
+    var output = ndarray(new Float32Array(nPlane * oH * oW),  [nPlane, oH, oW]);
 
     for (k = 0; k < nPlane; k++) {
 	/* loop over output */
-	long i, j;
+	var i, j;
 	for(i = 0; i < oH; i++) {
 	    for(j = 0; j < oW; j++) {
 		/* local pointers */
-		var *ip = input_p   + k*iwidth*iheight + i*iwidth*dH + j*dW;
-		var *op = output_p  + k*oW*oH + i*oW + j;
-		var *indyp = indy_p + k*oW*oH + i*oW + j;
-		var *indxp = indx_p + k*oW*oH + i*oW + j;
+		var ip = k*iwidth*iheight + i*iwidth*dH + j*dW;
+		var op = k*oW*oH + i*oW + j;
 
 		/* compute local max: */
-		long maxindex = -1;
-		var maxval = -THInf;
-		long tcntr = 0;
-		int x,y;
-		for(y = 0; y < kH; y++)
-		{
-		    for(x = 0; x < kW; x++)
-		    {
-			var val = *(ip + y*iwidth + x);
-			if (val > maxval)
-			{
+		var maxval = -999999;
+		var x,y;
+		for(y = 0; y < kH; y++) {
+		    for(x = 0; x < kW; x++) {
+			var val = input.data[ip + y*iwidth + x];
+			if (val > maxval) {
 			    maxval = val;
-			    maxindex = tcntr;
 			}
-			tcntr++;
 		    }
 		}
-
 		/* set output to local max */
-		    *op = maxval;
-
-		/* store location of max (x,y) */
-		    *indyp = (int)(maxindex / kW)+1;
-		    *indxp = (maxindex % kW) +1;
+		output.data[op] = maxval;
 	    }
 	}
     }
+    return output;
 }
+
+env.SpatialMaxPooling = SpatialMaxPooling
